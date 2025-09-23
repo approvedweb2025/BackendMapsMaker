c913377a4a42fb7cf3abdad7004ed68fe16bd329
approvedweb2025 <approvedweb2025@gmail.com>
2025-09-23 15:22:07 +0500

    googleDrive.js

--- utils/googleDrive.js
+++ utils/googleDrive.js
@@ -5,13 +5,8 @@ async function getFreshThumbnail(fileId, accessToken) {
   try {
     const response = await axios.get(
       `https://www.googleapis.com/drive/v3/files/${fileId}?fields=thumbnailLink`,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
+      { headers: { Authorization: `Bearer ${accessToken}` } }
     );
-
     return response.data.thumbnailLink;
   } catch (error) {
     console.error(`❌ Error refreshing thumbnail for ${fileId}:`, error?.response?.data || error.message);

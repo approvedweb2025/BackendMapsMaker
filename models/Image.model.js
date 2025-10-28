84e29440cc9c70b08ecf55dd633d0a67005c9823
approvedweb2025 <approvedweb2025@gmail.com>
2025-10-28 23:49:02 +0500

    Update Image.model.js

--- models/Image.model.js
+++ models/Image.model.js
@@ -1,16 +1,16 @@
+// models/Image.model.js
 const mongoose = require('mongoose');
 
 const ImageSchema = new mongoose.Schema({
   fileId: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   mimeType: { type: String, required: true },
-  //  Store image data directly in the database as a Buffer
   imageData: { type: Buffer, required: true },
-  latitude: { type: Number },
-  longitude: { type: Number },
+  latitude: { type: Number, default: null },
+  longitude: { type: Number, default: null },
   timestamp: { type: Date },
-  uploadedBy: { type: String },
-  lastCheckedAt: { type: Date },
+  uploadedBy: { type: String, required: true },
+  lastCheckedAt: { type: Date, default: Date.now },
   district: { type: String },
   tehsil: { type: String },
   village: { type: String },

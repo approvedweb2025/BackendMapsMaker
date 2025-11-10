b60a16debb52fed1ff1fc488e0ab35594008ed99
approvedweb2025 <approvedweb2025@gmail.com>
2025-11-10 13:35:12 +0500

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.use(cors({
   // ❗️ FRONTEND_URL ko environment variable se lein
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
   credentials: true,
+  optionsSuccessStatus: 200
 }));
 
 app.set('trust proxy', 1); 

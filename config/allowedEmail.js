9aace7d774c06165b78571deeca8aab791d63b86
approvedweb2025 <approvedweb2025@gmail.com>
2025-11-10 11:42:29 +0500

    allowedEmail.js

--- config/allowedEmail.js
+++ config/allowedEmail.js
@@ -1,6 +1,6 @@
 // allowedEmail.js
 export const allowedEmails = [
-    'mhuzaifa8519@gmail.com',
-    'mhuzaifa86797@gmail.com',
-    'muhammadjig8@gmail.com'
+    'peenaykapani@gmail.com',
+    'homesindh@gmail@gmail.com',
+    'demo@gmail.com'
 ];
